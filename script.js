76934c5aeeab0d156a78f78d1d74aedf20eeeaed
Adem Derinel <derinel@google.com>
2025-10-09 14:58:03 +0200

    Match the new form's id and name and make it hidden

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function showPasswordContainer() {
 
     let usernameField = '';
     if (!deleteUsername) {
-        usernameField = `<input id="email" value="${username}" autocomplete="email" class="hidden">`;
+        usernameField = `<type="hidden" input id="username" name="username" value="${username}" autocomplete="username" class="hidden">`;
     }
 
     const passwordContainerHTML = `
